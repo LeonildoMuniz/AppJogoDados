03dd0a4bf2eec0bda744348585b3f3e1b081df42
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2022-05-27 22:04:12 -0300

    Add files via upload

---
+++ AppJogoDados/PageJogo.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppJogoDados
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageJogo : ContentPage
+    {
+        public PageJogo()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            int n = r.Next(1, 7);
+            switch (n)
+            {
+                case 1:
+                    valor_dado.Source = "img1";
+                    break;
+
+                case 2:
+                    valor_dado.Source = "img2";
+                    break;
+
+                case 3:
+                    valor_dado.Source = "img3";
+                    break;
+
+                case 4:
+                    valor_dado.Source = "img4";
+                    break;
+
+                case 5:
+                    valor_dado.Source = "img5";
+                    break;
+
+                case 6:
+                    valor_dado.Source = "img6";
+                    break;
+
+            }
+        }
+
+        private async void btVoltar_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
